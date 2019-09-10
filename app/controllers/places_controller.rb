257ef5a0ffa38d547b4f4d833ba8de8e9cbfbbec
Simon Fuchs <Simon Fuchs>
2019-09-10 20:42:16 +0000

    Edit is working, redirects to root

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,6 +23,9 @@ class PlacesController < ApplicationController
   end
 
   def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   private
