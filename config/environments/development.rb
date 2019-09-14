cf24d417b81006f0ff827ef08c0ec5eae96e195c
Simon Fuchs <Simon Fuchs>
2019-09-14 04:21:01 +0000

    email pushed to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail,com",
+  Action_mailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: "example.com",
+    domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
